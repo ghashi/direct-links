f3628c5d485695c74d3408e215db788e0189ce2a
Guilherme Hashioka <guilherme.hashioka@gmail.com>
2020-08-05 15:21:30 -0300

    feat: 🎸 testing query-params fetch

--- js/src/forum/components/LoginPage.js
+++ js/src/forum/components/LoginPage.js
@@ -3,6 +3,16 @@ import LogInModal from 'flarum/components/LogInModal';
 
 export default class LoginPage extends RedirectToHomeAndOpenModalPage {
     createModal() {
+        try {
+            const params = m.route.param();
+            console.log(`LoginPage -> createModal -> params`, params);
+            const email = params.email
+            console.log(`LoginPage -> createModal -> email`, email);
+            const password = params.password
+            console.log(`LoginPage -> createModal -> password`, password);
+        } catch (error) {
+            console.log(`LoginPage -> createModal -> error`, error);
+        }
         return new LogInModal();
     }
 }
